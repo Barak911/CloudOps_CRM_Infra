11c0079d865c994d74bbe9859ea747bc4f52fa49
Barak Mooki <baraks_m4pro14@Baraks-MacBook-Pro.local>
2025-10-10 18:08:14 +0300

    Initial Terraform scaffolding: backend, provider, EKS, ECR

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-demo-crm"            # ← your S3 bucket
     key            = "cloudops_crm/terraform.tfstate"
-    region         = "us-east-1"                    # ← hard-coded region
+    region         = "il-center-1"                    # ← hard-coded region
     dynamodb_table = "terraform-locks"              # ← your DynamoDB lock table
     encrypt        = true
   }
