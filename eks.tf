283cdc34dbd340d3f03c447878d90c3f6c561b34
Barak911 <58632451+Barak911@users.noreply.github.com>
2025-12-03 20:06:08 +0200

    Change capacity type from SPOT to ON_DEMAND

--- eks.tf
+++ eks.tf
@@ -33,7 +33,7 @@ module "eks" {
       max_size       = 1
       min_size       = 1
       instance_types = ["t3a.medium"]
-      capacity_type  = "SPOT"
+      capacity_type  = "ON_DEMAND"
     }
   }
 
